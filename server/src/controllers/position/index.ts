dc9b9988d9274ef0c56c7dc133cb6691124d9087
Smile <xcxerxes@aliyun.com>
2018-10-11 08:26:55 +0800

    更新

--- server/src/controllers/position/index.ts
+++ server/src/controllers/position/index.ts
@@ -1,9 +1,14 @@
 import Position from '../../models/Position'
 import * as utils from '../../utils'
+import request from 'superagent'
 
 export async function getList (ctx: any): Promise<void> {
   const list = await Position.find({}).populate('companyId').limit(6)
   if (list) {
     ctx.body = {...utils.getStatusAndError({status: 200}), data: list}
   }
+}
+
+export async function getData (ctx: any): Promise<void> {
+  
 }
