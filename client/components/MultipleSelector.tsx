214153337c2f9684e9b74a1cfcaa7540d4668124
Smile <xcxerxes@aliyun.com>
2018-12-02 23:20:19 +0800

    更新公司页面

--- client/components/MultipleSelector.tsx
+++ client/components/MultipleSelector.tsx
@@ -1,36 +1,86 @@
-import React, { PureComponent, ReactChild, ReactElement } from 'react'
+import React, { PureComponent, Fragment } from 'react'
 
 type Props = {
   multiple?: boolean;
   selectorList?: Array<any>;
   selectedList?: Array<any>;
+  selectorClick: Function;
 }
 
 export default class MultipleSelector extends PureComponent<Props> {
   static defaultProps = {
-    multiple: false
+    multiple: true,
+    selectedList: []
   }
-  public renderItem = (item: Object) => {
+  constructor (props) {
+    super(props)
+    // if (props.multiple) {
+    //   this.state = {
+    //     multiSelectedList: [{title: '不限'}, ...props.selectedList]
+    //   }
+    // } else {
+    //   this.state = {
+    //     multiSelectedList: props.selectedList
+    //   }
+    // }
+  }
+  public isInclude (current) {
+    if (this.props.selectedList.findIndex(mult => mult.title === current.title) !== -1) {
+      return true
+    }
+    return false
+  }
+  public itemClick = (item: {title?: String}, index: Number) => {
+    this.props.selectorClick(item, index)
+  }
+  public renderItem = (item: {title?: String}, index: Number) => {
     const {multiple, selectedList} = this.props
-    if (multiple) {
-      const selectedList
+    if (multiple) {  
       return (
-        <div className="multiple-item__wrapper">
-          <span>{item.label}</span>
-          {selectedList.indexOf(item.label) !== -1 && <i></i>}
-        </div>
+        <li className={this.isInclude(item) ? 'multiple-item__wrapper active' : 'multiple-item__wrapper'} onClick={() => this.itemClick(item, index)}>
+          <span>{item.title}</span>
+          {this.isInclude(item) && <i></i>}
+          <style jsx scoped>{`
+          .multiple-item__wrapper {
+            padding: 4px 8px;
+            cursor: pointer;
+            transition: all .3s ease;
+          }
+          .multiple-item__wrapper.active {
+            background-color: #00b38a;
+            color: #fff;
+          }
+          .multiple-item__wrapper {
+            margin-left: 9px;
+            margin-right: 5px;
+          }
+          .multiple-item__wrapper.active>i {
+            display: inline-block;
+            width: 11px;
+            height: 11px;
+            background: url('/static/delete.png') center center no-repeat;
+            background-size: cover;
+            margin-left: 4px;
+          }
+          `}</style>
+        </li>
       )
     }
   }
   public render() {
     const {selectorList} = this.props
     return (
-      <div>
+      <div className="selector-wrapper">
         <ul>
           {selectorList.map((item: any, index: number) => (
-            <li key={index}>{this.renderItem(item)}</li>
+            <Fragment key={index}>{this.renderItem(item, index)}</Fragment>
           ))}
         </ul>
+        <style jsx scoped>{`
+          .selector-wrapper>ul {
+            display: flex;
+          }
+        `}</style>
       </div>
     )
   }
--- client/pages/company.tsx
+++ client/pages/company.tsx
@@ -1,15 +1,115 @@
 import React, { PureComponent } from 'react'
 import Layout from '../components/Layout'
+import MultipleSelector from '../components/MultipleSelector'
 
-class Company extends PureComponent {
+const FinancingOptions = [
+  {
+    title: '不限'
+  },
+  {
+    title: '未融资'
+  },
+  {
+    title: '天使轮'
+  },
+  {
+    title: 'A轮'
+  },
+  {
+    title: 'B轮'
+  },
+  {
+    title: 'C轮'
+  },
+  {
+    title: 'D轮及以上'
+  },
+  {
+    title: '上市公司'
+  },
+  {
+    title: '不需要融资'
+  }
+]
+type State = {
+  financingSelectList: Array<any>;
+}
+class Company extends PureComponent<State> {
+  state = {
+    financingSelectList: [{title: '不限'}]
+  }
+  // 判断当前的标签 是否在数组里面
+  private isInclude (list:Array<any>, current:{title: String}) {
+    if (list.findIndex(item => item.title === current.title) === -1) {
+      return false
+    }
+    return true
+  }
+  // 移除数组中的某一个
+  removeItem = (list, removeItem) =>{
+    const idx = list.findIndex(item => item.title === removeItem.title)
+    if (idx === -1) {
+      return list
+    }
+    return [...list.slice(0, idx), ...list.slice(idx + 1)]
+  }
+  // 未融资点击
+  financingClick = (item:{title: String}, index: Number) => {
+    const {financingSelectList} = this.state
+    if (item.title === '不限') {
+      this.setState({
+        financingSelectList: [{title: '不限'}]
+      })
+    } else {
+      const List = this.removeItem(financingSelectList, {title: '不限'})
+      if(this.isInclude(financingSelectList, item)) {
+        this.setState({
+          financingSelectList: this.removeItem(List, item)
+        })
+      } else {
+        this.setState({
+          financingSelectList: [...List, item]
+        })
+      }
+    }
+  }
   render() {
+    const {financingSelectList} = this.state
     return (
       <Layout>
         <section className="banner"></section>
+        <section className="filter-container">
+          <div className="filter-content">
+            <div className="filter-item">
+              <span>融资阶段：</span>
+              <MultipleSelector 
+              selectorList={FinancingOptions}
+              selectedList={financingSelectList}
+              selectorClick={this.financingClick}
+              multiple
+              />
+            </div>
+          </div>
+        </section>
         <style jsx>{`
         .banner {
           background-color: #00b38a;
           height: 387px;
+        }
+        .filter-container {
+          min-width: 1200px;
+        }
+        .filter-content {
+          width: 1200px;
+          margin: 0 auto;
+          padding: 0 15px;
+        }
+        .filter-item {
+          display: flex;
+          align-items: center;
+        }
+        .filter-item>span {
+          font-weight: 600;
         }
         `}</style>
       </Layout>
